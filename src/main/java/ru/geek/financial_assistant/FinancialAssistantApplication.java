7294f2dfb6267fb66daf8caab72307858657ae01
Kirill <benberov09@mail.ru>
2021-09-23 21:05:24 +0300

    Start_1

---
+++ src/main/java/ru/geek/financial_assistant/FinancialAssistantApplication.java
@@ -0,0 +1,13 @@
+package ru.geek.financial_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinancialAssistantApplication.class, args);
+    }
+
+}
